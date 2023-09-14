3f28b070a34427b3933d1fb93e5b0dc70e127ebd
arben777 <arbengb@gmail.com>
2023-09-13 21:06:25 -0700

    fixed config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 # This is a dictionary where keys are investor names and values are lists of example prompts for that investor.
 investor_specific_prompts = {
-    "Tilman Fertitta": {
+    "Marc Randolph": {
         "objectives": ["Launch a rare books subscription aiming for 10,000 subscribers in year one.", "Start an indie film streaming service, targeting 20 filmmaker collaborations in 6 months.", " Develop a podcast platform with exclusive content across genres."],
         "tasks": ["Negotiate with book dealers for a 50-box pilot with at least one first edition.", "Connect with 10 indie filmmakers for potential partnerships.", "Study top 5 podcast genres and list potential podcasters for the platform."]
     },
